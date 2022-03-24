0f1076f100a87b7a4945200b991d05f459b689ca
Geerx <90936654+GeerxOS@users.noreply.github.com>
2022-03-24 17:04:57 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ git clone https://github.com/GeerxOS/ip-info
 cd /usr/bin
 echo '#!/bin/bash' >> ip-info
 echo ' ' >> ip-info
-echo 'exec bash /usr/share/ip-info/ip-info.sh' >> io-info
+echo 'exec bash /usr/share/ip-info/ip-info.sh' >> ip-info
 echo 'Installed!'
 echo "Open terminal"
 echo "Use ip-info"

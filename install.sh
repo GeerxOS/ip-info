3fd96dc918a83fa5df165fdd6380dbb92ec6dd9e
Geerx <90936654+GeerxOS@users.noreply.github.com>
2022-03-24 17:02:57 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,3 +2,11 @@ sudo apt install curl
 cd /usr/share
 git clone https://github.com/GeerxOS/ip-info
 cd /usr/bin
+echo '#!/bin/bash' >> ip-info
+echo ' ' >> ip-info
+echo 'exec bash /usr/share/ip-info/ip-info.sh' >> io-info
+echo 'Installed!'
+echo "Open terminal"
+echo "Use ip-info"
+chmod 777 ip-info
+
